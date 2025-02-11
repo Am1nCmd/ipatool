56cbfdac85939c07f2a45a35b6cea55a713b02f0
^Amin Aenurahman <Shadow47_@msn.com>
2025-02-11 12:47:33 +0700

    enhance the download command to automatically handle the purchase if needed

--- cmd/download.go
+++ cmd/download.go
@@ -23,109 +23,7 @@ func downloadCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "download",
 		Short: "Download (encrypted) iOS app packages from the App Store",
-		RunE: func(cmd *cobra.Command, args []string) error {
-			if appID == 0 && bundleID == "" {
-				return errors.New("either the app ID or the bundle identifier must be specified")
-			}
-
-			var lastErr error
-			var acc appstore.Account
-			var purchased bool = false
-
-			return retry.Do(func() error {
-				infoResult, err := dependencies.AppStore.AccountInfo()
-				if err != nil {
-					return err
-				}
-
-				acc = infoResult.Account
-
-				if errors.Is(lastErr, appstore.ErrPasswordTokenExpired) {
-					loginResult, err := dependencies.AppStore.Login(appstore.LoginInput{Email: acc.Email, Password: acc.Password})
-					if err != nil {
-						return err
-					}
-
-					acc = loginResult.Account
-				}
-
-				app := appstore.App{ID: appID}
-				if bundleID != "" {
-					lookupResult, err := dependencies.AppStore.Lookup(appstore.LookupInput{Account: acc, BundleID: bundleID})
-					if err != nil {
-						return err
-					}
-
-					app = lookupResult.App
-				}
-
-				if errors.Is(lastErr, appstore.ErrLicenseRequired) {
-					err := dependencies.AppStore.Purchase(appstore.PurchaseInput{Account: acc, App: app})
-					if err != nil {
-						return err
-					}
-					purchased = true
-					dependencies.Logger.Verbose().
-						Bool("success", true).
-						Msg("purchase")
-				}
-
-				interactive, _ := cmd.Context().Value("interactive").(bool)
-				var progress *progressbar.ProgressBar
-				if interactive {
-					progress = progressbar.NewOptions64(1,
-						progressbar.OptionSetDescription("downloading"),
-						progressbar.OptionSetWriter(os.Stdout),
-						progressbar.OptionShowBytes(true),
-						progressbar.OptionSetWidth(20),
-						progressbar.OptionFullWidth(),
-						progressbar.OptionThrottle(65*time.Millisecond),
-						progressbar.OptionShowCount(),
-						progressbar.OptionClearOnFinish(),
-						progressbar.OptionSpinnerType(14),
-						progressbar.OptionSetRenderBlankState(true),
-						progressbar.OptionSetElapsedTime(false),
-						progressbar.OptionSetPredictTime(false),
-					)
-				}
-
-				out, err := dependencies.AppStore.Download(appstore.DownloadInput{Account: acc, App: app, OutputPath: outputPath, Progress: progress})
-				if err != nil {
-					return err
-				}
-
-				err = dependencies.AppStore.ReplicateSinf(appstore.ReplicateSinfInput{Sinfs: out.Sinfs, PackagePath: out.DestinationPath})
-				if err != nil {
-					return err
-				}
-
-				dependencies.Logger.Log().
-					Str("output", out.DestinationPath).
-					Bool("purchased", purchased).
-					Bool("success", true).
-					Send()
-
-				return nil
-			},
-				retry.LastErrorOnly(true),
-				retry.DelayType(retry.FixedDelay),
-				retry.Delay(time.Millisecond),
-				retry.Attempts(2),
-				retry.RetryIf(func(err error) bool {
-					lastErr = err
-
-					if errors.Is(err, appstore.ErrPasswordTokenExpired) {
-						return true
-					}
-
-					if errors.Is(err, appstore.ErrLicenseRequired) && acquireLicense {
-						return true
-					}
-
-					return false
-				}),
-			)
-		},
+		RunE: downloadRun,
 	}
 
 	cmd.Flags().Int64VarP(&appID, "app-id", "i", 0, "ID of the target iOS app (required)")
@@ -134,4 +32,28 @@ func downloadCmd() *cobra.Command {
 	cmd.Flags().BoolVar(&acquireLicense, "purchase", false, "Obtain a license for the app if needed")
 
 	return cmd
+}
+
+func downloadRun(cmd *cobra.Command, args []string) error {
+	if bundleID == "" {
+		return fmt.Errorf("bundle identifier is required")
+	}
+
+	// Check if the app is already purchased
+	isPurchased, err := store.IsPurchased(bundleID)
+	if err != nil {
+		return err
+	}
+
+	// If not purchased, attempt to purchase first
+	if !isPurchased {
+		fmt.Printf("App %s is not purchased. Attempting to purchase...\n", bundleID)
+		if err := store.Purchase(bundleID); err != nil {
+			return fmt.Errorf("failed to purchase app: %v", err)
+		}
+		fmt.Printf("Successfully purchased %s\n", bundleID)
+	}
+
+	// Proceed with download
+	return store.Download(bundleID, output)
 }
